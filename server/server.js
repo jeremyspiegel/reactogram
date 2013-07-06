86749ab4077d97d7b406d49ef25802f88b80d1ed
Brian Green <briangreenery@gmail.com>
2013-07-05 19:29:09 -0700

    Hello world server that accepts POSTS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
-var http = require('http');
+var express = require('express');
 var firebase = require('firebase');
 
-http.createServer(function(req, res) {
-  
+var app = express();
+
+app.use(express.bodyParser());
+
+app.post('/message', function(req,res) {
+  console.log(req.body);
+  res.send("ok");
 });
+
+app.listen(8080);
