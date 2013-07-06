b733cdeca8a6431ba0ac29ab06db4730317c2511
Brian Green <briangreenery@gmail.com>
2013-07-05 19:36:08 -0700

    Slightly more server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var firebase = require('firebase');
+var async = require('async');
 
 var app = express();
 
 app.use(express.bodyParser());
 
 app.post('/message', function(req,res) {
-  console.log(req.body);
+  var message = req.body;
+
+  console.log('send message: ' + message.message);
+
+  for (var i = 0; i < message.recipients.length; i++) {
+    console.log(message.recipients[i]);
+  }
+
   res.send("ok");
 });
 
